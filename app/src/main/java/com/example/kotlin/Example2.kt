190082a8efa22beb575ee2843001d24f253c0a6b
Jaeyong Kim <ajfnwodyd3@naver.com>
2023-06-14 21:57:42 +0900

    kotlin var,val practice

---
+++ app/src/main/java/com/example/kotlin/Example2.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin
+
+fun main(){
+    // 변수
+    // val = value(값)
+    // var = variable(변경가능한)
+    val a : Int =3 
+    var b : Int = 10
+    b= 20
+    
+    val name = "김재용" //타입 명시해주지않아도 코틀린은 추론
+}
