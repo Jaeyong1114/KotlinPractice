383b5e9b04c2ee9f8c6857df9b51885e7200f4bc
Jaeyong Kim <ajfnwodyd3@naver.com>
2023-08-02 16:55:37 +0900

    kotlin Constructor Practice

--- app/src/main/java/com/example/kotlin/Example3.kt
+++ app/src/main/java/com/example/kotlin/Example3.kt
@@ -9,14 +9,14 @@ fun main() {
  //상속
 open class User(open val name : String, open var age: Int = 100)
 
-class Kid(override val name: String, override var age: Int) : User(name,age){
+class Kid(override val name: String, override var age: Int) : User(name,age){ // 주생성자 Constructor 생략
     var gender : String = "female"
 
-    init {
+    init { //가장 먼저 호출
         println("초기화 중입니다")
     }
 
-    constructor(name : String, age: Int, gender: String) : this(name, age){
+    constructor(name : String, age: Int, gender: String) : this(name, age){ //부생성자
         this.gender = gender
         println("부생성자 호출")
     }
