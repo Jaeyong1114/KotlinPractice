1b783689c11785df30fbce77f3de0097f3db6934
Jaeyong Kim <ajfnwodyd3@naver.com>
2023-08-02 17:41:49 +0900

    kotlin Practice

---
+++ app/src/main/java/com/example/kotlin/Example5.kt
@@ -0,0 +1,44 @@
+package com.example.kotlin
+
+fun main() {
+   /* for(int i = 1; i< 11; i++){
+
+    }*/
+
+    for (i in 1..10){ // == for ( i in IntRange(1,10))
+        print(i)
+        print(".")
+
+    }
+
+    println()
+    for (i in 1 until 10 ){
+        print(i)
+        print(".")
+    }
+    println()
+    for (i in 1 ..10 step(2)){
+        print(i)
+        print(".")
+    }
+    println()
+
+    for (i in 10 downTo 1 ){
+        print(i)
+        print(".")
+    }
+    println()
+
+    for (i in 10 downTo 1 step(2)){
+        print(i)
+        print(".")
+    }
+    println()
+
+    var c = 1
+    while(c < 11){
+        print(c)
+        print(".")
+        c++
+    }
+}
